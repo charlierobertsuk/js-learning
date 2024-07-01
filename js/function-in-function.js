d1f5374aff050dfbff29961d4373457a4ab91f7e
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-01 21:02:25 +0100

    a function within a function

---
+++ js/function-in-function.js
@@ -0,0 +1,19 @@
+// alt up and down to move lines of code
+// ctrl D - selects all the word below that is identicle so all can be edited
+
+`strict mode`;
+
+function cutFriut (fruit) {
+    return fruit * 9;
+}
+
+const fruitProcessor = function(apples, oranges) {
+    
+    const applePieces = cutFriut(apples);
+    const orangePieces = cutFriut(oranges);
+    
+    const juice = `Juice with ${apples} apples cut into ${applePieces} pieces and ${oranges} oranges cut into ${orangePieces} pieces`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
