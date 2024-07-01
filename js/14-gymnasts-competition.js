161550e412e2fd56ecfde0ba206c3c1c454f6419
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-01 21:56:49 +0100

    gymnasts competition using functions

---
+++ js/14-gymnasts-competition.js
@@ -0,0 +1,20 @@
+`strict mode`;
+
+const dTotal = 44 + 23 + 71;
+const kTotal = 65 + 54 + 49;
+
+const calcAverage = (scoreDolphins,  scoreKoalas) => {
+    const dAverage = scoreDolphins/3;
+    const kAverage = scoreKoalas/3;
+    return checkWinner(dAverage, kAverage);
+}
+
+const checkWinner = function(avgDolphins, avgKoalas ) {
+    if (avgDolphins > avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas > avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    }
+}
+
+console.log(calcAverage(dTotal, kTotal));
