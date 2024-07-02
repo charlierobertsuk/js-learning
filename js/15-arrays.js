caeb3cc9f87bc7cba50393819657a192badc3eb4
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-02 20:29:13 +0100

    Arrays

---
+++ js/15-arrays.js
@@ -0,0 +1,11 @@
+`strict mode`;
+
+
+const friends= [`Mich`, `Ellie`, `Jeremy`];
+console.log(friends, friends[0], friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+const years = new Array(1967, 2013, 1945, 1816);
+console.log(years);
+
