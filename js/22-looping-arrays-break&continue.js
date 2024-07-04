e208a6ae2c10130089df03158691f67050734360
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-04 21:47:33 +0100

    for loop

---
+++ js/22-looping-arrays-break&continue.js
@@ -0,0 +1,13 @@
+`strict mode`;
+
+const charlieArray = [
+    `Charlie`,
+    `Roberts`,
+    2024 - 2007,
+    `student`,
+    [`Mich`, `Ellie`, `Jeremy`]
+];
+
+for (let i = 0; ; i+=1) {
+    console.log(charlieArray[0])
+}
