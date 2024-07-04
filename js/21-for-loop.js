6a2a8a3e9e134e97973b8f0eb661fcbb2a320473
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-04 21:34:27 +0100

    for loop

---
+++ js/21-for-loop.js
@@ -0,0 +1,22 @@
+`strict mode`;
+
+/* 
+console.log(`Lifting weights repitition 1`);
+console.log(`Lifting weights repitition 2`);
+console.log(`Lifting weights repitition 3`);
+console.log(`Lifting weights repitition 4`);
+console.log(`Lifting weights repitition 5`);
+console.log(`Lifting weights repitition 6`);
+console.log(`Lifting weights repitition 7`);
+console.log(`Lifting weights repitition 8`);
+console.log(`Lifting weights repitition 9`);
+console.log(`Lifting weights repitition 10`);
+
+ */
+
+
+// the for loop keeps running while condition is true
+// ++ is + 1
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repitition ${rep}`);
+}
