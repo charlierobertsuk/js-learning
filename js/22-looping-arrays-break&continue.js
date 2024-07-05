d06a4bd3af24fb8f27e20ebe5395ff1c66495189
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-05 18:48:21 +0100

    for loops are scary

--- js/22-looping-arrays-break&continue.js
+++ js/22-looping-arrays-break&continue.js
@@ -9,9 +9,20 @@ const charlie = [
 ];
 
 const types = [];
-
+const ages = [11, 17, 25, 42, 50];
+const years = [];
 
 for (let i = 0; i < charlie.length; i++) {
     types[i] = typeof charlie[i];
+    types.push(typeof charlie[i]);
+    console.log(types.length - 1);
     console.log(`${charlie[i]} is a ${types[i]}`);
 }
+/* 
+for (let i = 0; i < ages.length; i++) {
+    ages.push(2024 - ages[i]);
+}
+ */
+console.log(ages);
+console.log(ages.length);
+console.log(charlie.length);
