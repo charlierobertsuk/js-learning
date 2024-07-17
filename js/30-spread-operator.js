a68268c406e4345c3306876cd2c40e75d6095472
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-17 19:13:50 +0100

    spread operator

--- js/30-spread-operator.js
+++ js/30-spread-operator.js
@@ -33,6 +33,9 @@ const restaurant = {
       `Order recived! ${this.starterMenu[starterIndex]} and ${this.starterMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`This meal uses ${ing1}, ${ing2}, and ${ing3}`);
+  },
 };
 
 const arr = [7, 8, 9];
@@ -52,3 +55,18 @@ const mainMenuCopy = [...restaurant.mainMenu];
 //join 2 arrays
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 console.log(menu);
+
+// iterables - arrays, strings, ect. NOT objects though
+const str = `Charlie`;
+const letters = [...str, ``, `s.`];
+console.log(letters);
+console.log(...str);
+
+const ingredients = [
+  prompt(`Ingrediant 1`),
+  prompt(`Ingrediant 2`),
+  prompt(`Ingrediant 3`),
+];
+
+//restaurant.orderPasta(...ingredients);
+const newResturant = {foundedIn: 1998, ...restaurant, founder: `Guiseppe`}
