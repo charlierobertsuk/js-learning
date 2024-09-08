9efc627e4c7fa319bdd2fda2610356f14a9f3c98
Charlie Roberts <charlie@robertsclan.co.uk>
2024-09-08 20:01:44 +0100

    Football gambling

---
+++ js/32-football-gambling.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+/*
+- Football Betting App -
+
+Tasks:
+1. Create one player array for each team (variables `players1` and `players2`)
+2. The first player in any player array is the goalkeeper and the others are field players.
+   For Bayern Munich (team 1) create one variable (`gk`) with the goalkeeper's name, and one array (`fieldPlayers`) with all the remaining 10 field players
+3. Create an array `allPlayers` containing all players from both teams (22 players)
+4. During the game, Bayern Munich (team1)
+*/
