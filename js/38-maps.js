70bb4abb9896d81a94cc7a59501bbc333a034c50
Charlie Roberts <charlie@robertsclan.co.uk>
2024-10-25 12:18:10 +0100

    maps, add menu

--- js/38-maps.js
+++ js/38-maps.js
@@ -1,1 +1,59 @@
+`use strict`;
 
+const weekdays = [`mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `sun`];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[3 + 1]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0,
+    close: 24,
+  },
+};
+
+const restaurant = {
+  title: `Classico Italiano`,
+  location: `Via Angelo Tavanti 23, Firenze, Italy`,
+  categories: [`Italian`, `Pizzeria`, `Vegetarian`, `Organic`],
+  starterMenu: [`Focaccia`, `Brushetta`, `Garlic Bread`, `Caprese Salad`],
+  mainMenu: [`Pizza`, `Pasta`, `Risotto`],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function (
+    starterIndex = 1,
+    mainIndex = 0,
+    time = `20:00`,
+    address
+  ) {
+    console.log(
+      `Order recived! ${this.starterMenu[starterIndex]} and ${this.starterMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`This meal uses ${ing1}, ${ing2}, and ${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+};
