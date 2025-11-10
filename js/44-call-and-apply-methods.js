77f289492452174af75f738240d9b7a525fba0b9
charlierobertsuk <charlie@robertsclan.co.uk>
2025-11-10 23:21:58 +0000

    Start of call and apply challenge

--- js/44-call-and-apply-methods.js
+++ js/44-call-and-apply-methods.js
@@ -62,4 +62,37 @@ bookLX(56, "Timmy Bartholomew");
 const bookEW23 = book.bind(eurowings, 23);
 bookEW23("Sally Wiggens");
 
-// With event listeners
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+console.log("Coding Challenge:");
+
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "4: C++"],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    console.log(this.question);
+    this.options.forEach(optionsList);
+  },
+};
+
+const optionsList = (str) => console.log(str);
+
+poll.registerNewAnswer();
